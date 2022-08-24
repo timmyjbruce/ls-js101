7958724c31b83ab55b96796bb77de81c0f202158
timmyjbruce <timmyjbruce@gmail.com>
2022-08-24 10:58:06 +0100

    Added features and updated calulator logic

--- lesson-2/calculator.js
+++ lesson-2/calculator.js
@@ -1,55 +1,78 @@
+const readline = require('readline-sync');
+const MESSAGES = require('./calculator-messages.json');
+let lang = "en";
+
 function promptedMessage(message) {
   console.log(`=> ${message}`);
 }
-
-const readline = require('readline-sync');
-
-promptedMessage('Welcome to Calculator!');
-
-promptedMessage("What's the first number?");
-let number1 = readline.question();
-
-while (invalidNumber(number1)) {
-  promptedMessage("Hmm... that doesn't look like a valid number. Please try again");
-  number1 = readline.question();
-}
-
 function invalidNumber(value) {
-  return number1.trimStart === '' || Number.isNaN(Number(value));
+  return value.trimStart === '' || Number.isNaN(Number(value));
 }
 
-promptedMessage("What's the second number?");
-let number2 = readline.question();
+promptedMessage(MESSAGES[lang].language);
+lang = readline.question().toLowerCase();
 
-while (invalidNumber(number2)) {
-  promptedMessage("Hmm... that doesn't look like a valid number. Please try again");
-  number2 = readline.question();
+while ((lang !== 'en') && (lang !== 'es')) {
+  promptedMessage(MESSAGES[lang].valid);
+  lang = readline.question();
 }
 
-promptedMessage('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operation = readline.question();
+while (true) {
+
+  promptedMessage(MESSAGES[lang].q1);
+  let number1 = readline.question();
+
+  while (invalidNumber(number1)) {
+    promptedMessage(MESSAGES[lang].q1);
+    number1 = readline.question();
+  }
+
+  promptedMessage(MESSAGES[lang].q2);
+  let number2 = readline.question();
+
+  while (invalidNumber(number2)) {
+    promptedMessage(MESSAGES[lang].q2);
+    number2 = readline.question();
+  }
+
+  promptedMessage(MESSAGES[lang].q3);
+  let operation = readline.question();
 
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  promptedMessage('Invalid input. Please select one of the following: \n1) Add 2) Subtract 3) Multiply 4) Divide');
-  operation = readline.question();
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    promptedMessage(MESSAGES[lang].invalid);
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1' :
+      output = Number(number1) + Number(number2);
+      break;
+    case '2' :
+      output = Number(number1) - Number(number2);
+      break;
+    case '3' :
+      output = Number(number1) * Number(number2);
+      break;
+    case '4' :
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  promptedMessage(MESSAGES[lang].result + output);
+
+  promptedMessage(MESSAGES[lang].again);
+  let repeat = readline.question();
+
+  while ((repeat.toLowerCase() !== 'n') && (repeat.toLowerCase() !== 'y')) {
+    promptedMessage(MESSAGES[lang].valid);
+    repeat = readline.question();
+  }
+  if (repeat === 'n') {
+    promptedMessage(MESSAGES[lang].thanks);
+    break;
+  } else if (repeat === 'y') {
+    promptedMessage(MESSAGES[lang].restart);
+  }
 }
-
-let output;
-switch (operation) {
-  case '1' :
-    output = Number(number1) + Number(number2);
-    break;
-  case '2' :
-    output = Number(number1) - Number(number2);
-    break;
-  case '3' :
-    output = Number(number1) * Number(number2);
-    break;
-  case '4' :
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-promptedMessage(`The result is: ${output}`);
-
